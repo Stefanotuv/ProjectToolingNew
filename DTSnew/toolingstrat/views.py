d01aac9d24f92ffcee1553062919680acc07fc41
stefanotuv <ste.tuveri@gmail.com>
2019-01-04 22:44:29 +0000

    second

--- DTSnew/toolingstrat/views.py
+++ DTSnew/toolingstrat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def index(request):
 # help page in the template folder
     return HttpResponse("Toolingstrat view!")
